93e828be654b684f5ae506679857268753d38ed6
labartbe <76131360+labartbe@users.noreply.github.com>
2023-06-15 17:29:38 +0200

    version a envoyer au prof

--- Views/view_accueil.php
+++ Views/view_accueil.php
@@ -1,9 +1,7 @@
 <main>
   <section id="hero">
-  <h1>Bienvenue sur la page d'accueil</h1>
-<h3>Ici il y aura le choix du theme, du niveau et pour commencer la partie</h3>
-<a class="afficher_theme" href="?controller=selection&action=all_themes">Afficher les themes</a>
-</div>
-
+    <h1>Bienvenue/h1>
+    <h3>Ici il y aura le choix du thème, du niveau et pour commencer la partie.</h3>
+    <a class="btn_afficher_theme btn" href="?controller=selection&action=all_themes">Afficher les thèmes</a>
   </section>
 </main>
